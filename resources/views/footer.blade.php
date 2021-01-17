2024e9f0308f9a0f8b339e6bdb5643e0b48018df
Your NamePhuongNam160899 <you@example.comhaphuongnam81999@gmail.com>
2021-01-17 15:32:58 +0700

    delte

--- resources/views/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="footer-body">
-	<div class="copyright"><p>Copyright &#169; 2020</p><a href="webamthucdnc.000webhostapp.com">webamthucdnc.000webhostapp.com</a>
-	</div>
-	<div class="copyright"><p>Địa chỉ : Hà Nội</p></div>
-	<div class="copyright"><p>Liên hệ :</p></div>
-	<div class="copyright"><p>-SDT: 0964712946</p></div>
-	<div class="copyright"><p>-Gmail: haphuongnam@gmail.com</p></div>
-	<div class="copyright"><p>-Facebook:</p><a href="webamthucdnc.000webhostapp.com">Phương Nam</a></div>
-</div>
